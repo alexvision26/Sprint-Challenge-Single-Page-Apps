e3c6c3f4130e8350104ae89733d75026b98d34da
Alex Miller <alexmillerprojects@gmail.com>
2020-01-31 08:54:30 -0800

    finished MVP, working ons stretch

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -4,16 +4,38 @@ import axios from 'axios';
 export default function CharacterList() {
   const [characters, setCharacters] = useState([]);
 
+  const [query, setQuery] = useState('');
+
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     axios.get('https://rickandmortyapi.com/api/character/').then(res => {
-      console.log(res.data.results)
-      setCharacters(res.data.results);
+      // console.log(res.data.results)
+      // setCharacters(res.data.results);
+      const newChars = res.data.results.filter(char => char.name.toLowerCase().includes(query.toLowerCase())
+      );
+      setCharacters(newChars)
     })
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+  }, [query]);
+
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  }
 
   return (
+    <div className='container'>
+      <form className='search-bar'>
+        <label htmlFor='name'>Search: </label>
+        <input
+          type='text'
+          onChange={handleInputChange}
+          value={query}
+          name='name'
+          placeholder='Search by name...'
+          autoComplete='off'
+          />
+
+      </form>
     <section className="character-list">
       {characters.map(e => {
         return (
@@ -25,5 +47,6 @@ export default function CharacterList() {
         )
       })}
     </section>
+    </div>
   );
 }
