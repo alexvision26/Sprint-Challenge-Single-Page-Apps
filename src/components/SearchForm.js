5f8b8c3dba2c4718ad20cd6df533ef06520f72cf
Alex Miller <alexmillerprojects@gmail.com>
2020-01-31 08:42:38 -0800

    working on search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,8 +4,7 @@ export default function SearchForm() {
  
   return (
     <section className="search-form">
-     <label htmlFor='search'>Search: </label>
-     <input id='search' type='text' placeholder='Search...'/>
+     
     </section>
   );
 }
